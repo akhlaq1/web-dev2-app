c191cf8b7c8c27d75ccd11793d877989a0207006
Akhlaq Ahmed <engr.akhlaq@hotmail.com>
2022-09-16 19:55:54 +0500

    react router class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
-import Book from './components/Book'
+import React, {useState, useEffect} from "react";
+import {
+    Routes,
+    Route,
+  } from "react-router-dom";
+import Main from "./components/Main";
+import About from "./components/About";
+import ShoppingCart from './components/shopping/ShoppingCart'
+import Payment from './components/shopping/Payment'
+import Checkout from './components/shopping/Checkout'
+
+
 
 function App(){
-    
+  
     return (
-        <>
-        <Book />
-        </>
-      
-    
-    )
+          <Routes>
+            <Route path="/" element={<Main/>}></Route>
+
+
+            /about/shopping
+            <Route path="/about" element={<About/>} >
+                
+                <Route path="shopping" element={<ShoppingCart/>}></Route>
+                <Route path="checkout" element={<Checkout/>}></Route>
+                <Route path="payment" element={<Payment/>}></Route>
+            
+            </Route>
+          
+          
+          
+          </Routes>
+      );
 }
 
 export default App
